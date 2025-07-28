163c4689d85fbffc59abc8e0e0e09ebf871fd6bc
aymanabdeenn <151556377+aymanabdeenn@users.noreply.github.com>
2025-07-28 12:06:17 +0300

    Initial files

---
+++ src/main/java/com/example/uniSystem_web_app/UniSystemWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.uniSystem_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSystemWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniSystemWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uniSystem_web_app/UniSystemWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uniSystem_web_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSystemWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
